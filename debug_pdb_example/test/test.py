717c284793be595bd11864ac1b82bffc00f95339
Phung Xuan Anh <anhpx@viosoft.com>
2017-12-23 11:18:41 +0000

    Add sample for pydev remove debugger

--- debug_pdb_example/test/test.py
+++
@@ -1,9 +0,0 @@
-class Test(object):
-    def print_a(self):
-        print("aaaaaaaaaaaaaaaaaaa")
-        
-    def print_b(self):
-        print("bbbbbbbbbbbbbbbbbbb")
-        
-    def raise_erro(self):
-        raise Exception("eeeeeeeeeeeeeee")        
