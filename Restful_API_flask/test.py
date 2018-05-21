02dee179473a77df3428bf16915d01abf14dc7dc
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-05-21 10:57:59 +0700

    Change code

---
+++ Restful_API_flask/test.py
@@ -0,0 +1,13 @@
+import requests
+
+r = requests.get('http://localhost:5000/product/')
+
+# r = requests.post('http://localhost:5000/product/',
+#                   data={'name': 'iPhone 6s', 'price': 699})
+
+# r = requests.post('http://localhost:5000/product/', 
+#                   data={'name': 'iPad Pro', 'price': 999})
+
+print r.json()
+
+
