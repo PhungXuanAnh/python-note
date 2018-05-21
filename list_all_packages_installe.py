f2b2a95aa00cd656183be26babd93da22d01e69a
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-05-21 11:10:49 +0700

    deleted code

--- list_all_packages_installe.py
+++
@@ -1,5 +0,0 @@
-import pip, json
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-     for i in installed_packages])
-print json.dumps(installed_packages_list, indent=4, sort_keys=True)
