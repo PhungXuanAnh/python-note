696ff54e5a07faf7781f12333be5ad945dcc7194
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-06 13:56:20 +0700

    modified code

--- time_sample/time_sample.py
+++ time_sample/time_sample.py
@@ -3,6 +3,18 @@ Created on Jun 26, 2017
 
 @author: xuananh
 '''
+
+import dateutil.parser
+print(dateutil.parser.parse('2008-09-03T20:56:35.450686Z')) # RFC 3339 format
+datetime.datetime(2008, 9, 3, 20, 56, 35, 450686, tzinfo=tzutc())
+print(dateutil.parser.parse('2008-09-03T20:56:35.450686')) # ISO 8601 extended format
+datetime.datetime(2008, 9, 3, 20, 56, 35, 450686)
+print(dateutil.parser.parse('20080903T205635.450686')) # ISO 8601 basic format
+datetime.datetime(2008, 9, 3, 20, 56, 35, 450686)
+print(dateutil.parser.parse('20080903')) # ISO 8601 basic format, date only
+datetime.datetime(2008, 9, 3, 0, 0)
+
+
 import datetime
 import time
 
@@ -22,7 +34,3 @@ period = (now - start).seconds
 print("44444444444444444 period = {}".format(period))
 
 
-import dateutil.parser
-datestring= "2018-06-06T04:52:15.526Z"
-yourdate = dateutil.parser.parse(datestring)
-print(yourdate)
