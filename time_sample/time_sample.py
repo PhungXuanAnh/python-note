7a8af7dd8c7ff14cacfcd0fbd47fc2ab852adf3b
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-06 15:11:30 +0700

    modified code

--- time_sample/time_sample.py
+++ time_sample/time_sample.py
@@ -20,9 +20,9 @@ print(datetime.datetime(2008, 9, 3, 20, 56, 35, 450686))
 print(dateutil.parser.parse('20080903')) # ISO 8601 basic format, date only
 print(datetime.datetime(2008, 9, 3, 0, 0))
 
-print(dateutil.parser.parse("2018-06-06T08:01:53.420Z")[0:18])
+print(dateutil.parser.parse("2018-06-06T08:01:53.420Z").isoformat = 'YYYY-MM-DD HH:MM:SS')
 
-print(datetime.datetime.strptime("2008-09-03 20:56:35.450686+00:00", '%Y-%m-%d %H:%M:%S'))
+# print(datetime.datetime.strptime("2008-09-03 20:56:35.450686+00:00", '%Y-%m-%d %H:%M:%S'))
 
 
 print('==============================')
