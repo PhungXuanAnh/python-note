734ef005cb5f88a3e353697afcfccd1052c76ddd
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-09-18 11:50:29 +0700

    add time module

--- test.py
+++ test.py
@@ -1,2 +1,74 @@
-list1 = "abcdefghij\n"
-print(list1[:-1])
+'''
+Viet code va testcase tao ra 100 nguoi dung lien tiep vang mat hoac lien tiep co mat va in ra
+
+- tong so nguoi co mat moi ngay va id
+- tong so nguoi vang mat moi ngay va id
+- tong so nguoi dung co mat 2 ngay lien tiep va id
+- tong so nguoi dung mat mat 2 ngay lien tiep va id
+
+model:
+- user_id - not null
+- date login - not null
+- time login - maybe null
+- status: present/absent - not null
+
+user_id: {
+    date:
+    time:
+    status:
+}
+
+user_id = [0, ..., 99]
+date:  [user_id, user_id, user_id ]
+'''
+
+import random
+
+
+def genarate_user():
+    '''
+    - tao 100 nguoi dung ngau nhien co mat hoac vang mat lien tiep 2 ngay
+    - tao trong 30 ngay
+    '''
+    counts_present = random.randint(0, 100)
+    list_id_present = random.sample(range(0, 99), counts_present)
+
+    for i in range(1, 30):
+        write_database(i: list_id_present)
+
+
+list_present_consecutive_day = []
+list_absent_consecutive_day = []
+
+list_id_present_tmp = []
+list_id_absent_tmp = []
+for i in range(1, 30):
+    list_id_present = get_date(i)
+
+    # in ra tong so nguoi co mat hang ngay va id
+    print(len(list_id_present), list_id_present)
+
+    list_id_absent = list(range(0, 99))
+    for id in list_id_present:
+        list_id_absent.remove(id)
+
+    # in ra tong so nguoi vang mat hang ngay va id
+    print(len(list_id_absent), list_absent)
+
+    for id in list_id_present_tmp:
+        if id in list_id_present:
+            list_present_consecutive_day.append(id)
+
+    for id in list_id_absent_tmp:
+        if id in list_id_absent:
+            list_absent_consecutive_day.append(id)
+
+    list_id_present_tmp = list_id_present
+    list_id_absent_tmp = list_id_absent
+
+
+list_present_consecutive_day = list(set(list_present_consecutive_day))
+list_absent_consecutive_day = list(set(list_absent_consecutive_day))
+
+print(len(list_present_consecutive_day), list_present_consecutive_day)
+print(len(list_absent_consecutive_day), list_absent_consecutive_day)
--- time_sample/time_sample1.py
+++ time_sample/time_sample1.py
@@ -4,59 +4,58 @@ Created on Jun 26, 2017
 @author: xuananh
 '''
 import datetime
+import calendar
 import time
-import dateutil.parser # pip install python-dateutil
+import dateutil.parser  # pip install python-dateutil
 from dateutil.tz import tzutc
 
-print(dateutil.parser.parse('2008-09-03T20:56:35.450686Z')) # RFC 3339 format
-print(datetime.datetime(2008, 9, 3, 20, 56, 35, 450686, tzinfo=tzutc()))
+print('----------------------------------------------------------- convert other format to python datetime')
+print('UTC timestamp now : ', time.time())
+print('UTC timestamp now : ', datetime.datetime.now().timestamp())
+print('UTC timestamp now : ', calendar.timegm(time.gmtime()))
+print('UTC timestamp to datetime :       ', datetime.datetime.fromtimestamp(1536775161))
+print('UTC timestamp to datatime - 24h : ',
+      datetime.datetime.fromtimestamp(1536775161 - 24 * 60 * 60))
 
-print(dateutil.parser.parse('2008-09-03T20:56:35.450686')) # ISO 8601 extended format
-print(datetime.datetime(2008, 9, 3, 20, 56, 35, 450686))
+print('RFC 3339 format: ', dateutil.parser.parse('2008-09-03T20:56:35.450686Z'))
+print('                 ', datetime.datetime(2008, 9, 3, 20, 56, 35, 450686, tzinfo=tzutc()))
 
-print(dateutil.parser.parse('20080903T205635.450686')) # ISO 8601 basic format
-print(datetime.datetime(2008, 9, 3, 20, 56, 35, 450686))
+print('ISO 8601 extended format: ', dateutil.parser.parse('2008-09-03T20:56:35.450686'))
+print('                          ', datetime.datetime(2008, 9, 3, 20, 56, 35, 450686))
 
-print(dateutil.parser.parse('20080903')) # ISO 8601 basic format, date only
-print(datetime.datetime(2008, 9, 3, 0, 0))
+print('ISO 8601 basic format: ', dateutil.parser.parse('20080903T205635.450686'))
+print('                       ', datetime.datetime(2008, 9, 3, 20, 56, 35, 450686))
 
+print('ISO 8601 basic format, date only: ', dateutil.parser.parse('20080903'))
+print('                                  ', datetime.datetime(2008, 9, 3, 0, 0))
+
+print('----------------------------------------------------------- format datetime')
 my_time = dateutil.parser.parse("2018-06-06T08:01:53.420Z")
-print(my_time)
-print(my_time.strftime("%Y-%m-%d %H:%M:%S"))
-
-# print(datetime.datetime.strptime("2008-09-03 20:56:35.450686+00:00", '%Y-%m-%d %H:%M:%S'))
+print('my_time:          ', my_time)
+print("my_time formated: ", my_time.strftime("[%Y-%m-%d]-[%H:%M:%S]"))
 
 
-print('==============================')
-timestr = time.strftime("[%Y-%m-%d]-[%H:%M:%S]")
-print("111111111111111111 {}".format(timestr))
-
-#=============================================
+print('----------------------------------------------------------- period')
 start = datetime.datetime.now()
-print("22222222222222222 start = {}".format(start))
-print("22222222222222222 start = {}".format(start.strftime("[%Y-%m-%d]-[%H:%M:%S]")))
+print("start:          ", start)
 
 print('....spleeping 3s...')
 time.sleep(3)
- 
+
 now = datetime.datetime.now()
-print("33333333333333333 now = {}".format(now))
-print("33333333333333333 now = {}".format(now.strftime("[%Y-%m-%d]-[%H:%M:%S]")))
+print("now:          ", now)
 
 period = (now - start).seconds
-print("44444444444444444 period = {}".format(period))
+print("period: ", period)
 
-datetime_7_day_ago = datetime.datetime.now() - datetime.timedelta(days=7)
-print("55555555555555555 date_7_day_ago = {}".format(datetime_7_day_ago))
-print("55555555555555555 now = {}".format(datetime_7_day_ago.strftime("[%Y-%m-%d]-[%H:%M:%S]")))
+print('----------------------------------------------------------- datetime ago')
+now = datetime.datetime.now()
+datetime_7_day_ago = now - datetime.timedelta(days=7)
+print("now :           ", now)
+print("date_7_day_ago: ", datetime_7_day_ago)
 
 today = datetime.date.today()
-print('666666666 today = {}'.format(today))
-
 date_7_day_ago = today - datetime.timedelta(days=7)
-print('666666666 date_7_day_ago = {}'.format(date_7_day_ago))
-
-
-
-
+print('today           ', today)
+print('date_7_day_ago: ', date_7_day_ago)
 
