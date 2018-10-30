1252026c0074074f0dc682f36a48cf335b8d3b94
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-10-30 16:55:51 +0700

    Add code

---
+++ selenium_sample/selenium_sample2.py
@@ -0,0 +1,30 @@
+import time
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.keys import Keys
+
+
+chrome_option = ChromeOptions()
+# chrome_option.set_headless()
+
+driver = Chrome(executable_path='/home/xuananh/Downloads/chromedriver_linux64/chromedriver',
+                chrome_options=chrome_option)
+
+driver.maximize_window()
+driver.get('https://www.facebook.com/K14vn')
+
+
+# scroll to the end of page
+is_found = False
+
+while not is_found:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+
+    els = driver.find_elements_by_xpath('//abbr')
+    for el in els:
+        if int(el.get_attribute("data-utime")) == 1530171486:
+            is_found = True
+            break
+
+# driver.close()
