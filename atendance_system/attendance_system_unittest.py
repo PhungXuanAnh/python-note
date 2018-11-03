cda550dd652032a8bafbf2d029b43155e4f5bc38
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-11-03 22:13:45 +0700

    add code

--- atendance_system/attendance_system_unittest.py
+++ atendance_system/attendance_system_unittest.py
@@ -5,11 +5,11 @@ import redis
 
 class UnittestAttendaceSystem(unittest.TestCase):
 
-    def test_pass_get_redis_client(self):
+    def test_get_redis_client(self):
+        # pass
         r_client = at.get_redis_client()
         self.assertIsInstance(r_client, redis.StrictRedis)
-
-    def test_fail_get_redis_client(self):
+        # fail
         r_client = at.get_redis_client(host="1.1.1.1")
         self.assertEqual(r_client, None)
         r_client = at.get_redis_client(port=1234)
@@ -17,6 +17,14 @@ class UnittestAttendaceSystem(unittest.TestCase):
         r_client = at.get_redis_client(db=20)
         self.assertEqual(r_client, None)
 
+    def test_generate_attendance_randomly(self):
+        # pass
+        self.assertTrue(at.generate_attendance_randomly(2011, 1, 1, 100))
+        # fail
+        self.assertFalse(at.generate_attendance_randomly(-2018, 1, 1, 100))
+        self.assertFalse(at.generate_attendance_randomly(2018, 15, 1, 100))
+        self.assertFalse(at.generate_attendance_randomly(2018, 1, 100, 100))
+        self.assertFalse(at.generate_attendance_randomly(2018, 1, 1, 0))
 
 if __name__ == '__main__':
     unittest.main()
