8975051b72d1021cbbcbf54bb1c2fb682550f5c5
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-05-06 16:52:21 +0700

    Change code sample of rabbitmq

--- rabbitmq/receive.py
+++ rabbitmq/receive.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python
 import pika
+import json
 
-# import pdb
-# pdb.set_trace()
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='10.64.0.167'))
+QUEUE = 'result'
+# HOST = 'localhost'
+HOST = '188.166.238.142'
+PORT = 5673
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST, port=PORT))
 channel = connection.channel()
 
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue=QUEUE)
+
 
 def callback(ch, method, properties, body):
-    print (body)
-#     print(" [x] Received %r" % body)
+    print(json.dumps(json.loads(body), indent=4, sort_keys=True))
+    print('--------------------------------------------------')
 #     ch.close()  # this line to close connection right after receive message
 
 # channel.basic_consume(consumer_callback, queue, no_ack, exclusive, consumer_tag, arguments)
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+channel.basic_consume(queue=QUEUE, on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- rabbitmq/send.py
+++ rabbitmq/send.py
@@ -2,30 +2,30 @@
 import pika
 import json
 
+
 def send(host_ip, queue, message):
     connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=host_ip))
+        host=host_ip))
     channel = connection.channel()
-    
-    
+
     channel.queue_declare(queue=queue)
-    
+
     channel.basic_publish(exchange='',
                           routing_key=queue,
                           body=message)
     print(" [x] Sent '{}'".format(message))
     connection.close()
 
+
 if __name__ == '__main__':
-    host_ip = '10.64.0.167'
-#     queue = 'centos-10.64.0.156'
-    queue = '11111'
+    host_ip = 'localhost'
+    queue = '111'
     message = {
-		  			"SessionId": "123",
-# 					"Command": "lock_run_test",
-# 					"Command": "unlock_run_test",
-					"Command": "check_run_test",
-                    "Params": {},
-                    "From": "test"}    
-    
+        "SessionId": "123",
+        # 					"Command": "lock_run_test",
+        # 					"Command": "unlock_run_test",
+        "Command": "check_run_test",
+        "Params": {},
+        "From": "test"}
+
     send(host_ip, queue, json.dumps(message))
