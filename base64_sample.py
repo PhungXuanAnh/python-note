3d8681007695588be12940c1141e14bbc34e2e74
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-07-01 17:15:13 +0700

    add sample for base64 encode and decode

---
+++ base64_sample.py
@@ -0,0 +1,10 @@
+import base64
+
+string = u'complex string: ñáéíóúÑ'
+string = '123'
+string = 'hà nội'
+
+a = base64.b64encode(bytes(string, "utf-8"))
+b = base64.b64decode(a).decode("utf-8", "ignore")
+
+print(b)
