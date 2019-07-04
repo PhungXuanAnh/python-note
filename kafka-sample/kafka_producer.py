44727c3cd77f56fa348f5ff13e025c58007d9804
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-07-04 17:59:46 +0700

    update kafka sample

--- kafka-sample/kafka_producer.py
+++ kafka-sample/kafka_producer.py
@@ -3,58 +3,69 @@ import traceback
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
-# asynchronous by default
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-future = producer.send('my-topic', b'raw_bytes')
+# topic = 'my-topic'
+topic = 'facebook-items'
 
-# block for 'synchronous' sends
-try:
-    record_metadata = future.get(timeout=10)
-except KafkaError:
-    # decide what todo if produce request failed...
-    traceback.print_exc()
+kafka_servers = ['localhost:9092']
 
-# successfully result returns assigned partition and offset
-print(record_metadata.topic)
-print(record_metadata.partition)
-print(record_metadata.offset)
+# # -----------------------------------------------------
+# # # asynchronous by default
+# producer = KafkaProducer(bootstrap_servers=kafka_servers)
+# future = producer.send(topic, b'raw_bytes')
 
-# produce keyed message to enable hashed partitioning
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-producer.send('my-topic', key=b'foo', value=b'bar')
+# # block for 'synchronous' sends
+# try:
+#     record_metadata = future.get(timeout=10)
+# except KafkaError:
+#     # decide what todo if produce request failed...
+#     traceback.print_exc()
 
-# encode objects via msgpack
+# # successfully result returns assigned partition and offset
+# print(record_metadata.topic)
+# print(record_metadata.partition)
+# print(record_metadata.offset)
+
+# # -----------------------------------------------------
+# # produce keyed message to enable hashed partitioning
+# producer = KafkaProducer(bootstrap_servers=kafka_servers)
+# producer.send(topic, key=b'foo', value=b'bar')
+
+# # -----------------------------------------------------
+# # encode objects via msgpack
 # producer = KafkaProducer(value_serializer=msgpack.dumps)
 # producer.send('msgpack-topic', {'key': 'value'})
 
-# produce json message
-# producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('ascii'))
-# producer.send('json-topic', {'key': 'value'})
+# -----------------------------------------------------
+# # produce json message
+producer = KafkaProducer(
+    bootstrap_servers=kafka_servers,
+    value_serializer=lambda m: json.dumps(m).encode('ascii'))
+producer.send(topic, {'key': 'value'})
 
 # produce asynchronously
-# producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+# producer = KafkaProducer(bootstrap_servers=kafka_servers)
 # for _ in range(100):
 #     producer.send('my-topic', b'msg')
 
 
-def on_send_success(record_metadata):
-    print(record_metadata.topic)
-    print(record_metadata.partition)
-    print(record_metadata.offset)
+# def on_send_success(record_metadata):
+#     print(record_metadata.topic)
+#     print(record_metadata.partition)
+#     print(record_metadata.offset)
 
 
-def on_send_error(excp):
-    print(excp)
+# def on_send_error(excp):
+#     print(excp)
 
 
-# produce asynchrously with callbacks
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-producer.send('my-topic', b'raw_bytes')\
-    .add_callback(on_send_success)\
-    .add_errback(on_send_error)
+# # produce asynchrously with callbacks
+# producer = KafkaProducer(bootstrap_servers=kafka_servers)
+# producer.send(topic, b'raw_bytes')\
+#     .add_callback(on_send_success)\
+#     .add_errback(on_send_error)
 
-# block until all async messages are sent
+# # block until all async messages are sent
 producer.flush()
 
-# configure multiple retries
-producer = KafkaProducer(retries=5)
+# # configure multiple retries
+# producer = KafkaProducer(retries=5)
