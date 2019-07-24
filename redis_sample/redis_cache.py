c559f8e272811e1aeb61f70d4481806d6731b2e4
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-07-24 11:21:09 +0700

    update redis sample

--- redis_sample/redis_cache.py
+++ redis_sample/redis_cache.py
@@ -5,8 +5,11 @@ r = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 key = 'aaa'
 data = {'key1': 'value1'}
-time_expire = 10
+time_expire = 100
 
 r.setex(key, time_expire, json.dumps(data))
 
-print(r.get(key))
+value = r.get(key)
+value = value.decode('ascii')
+print(type(value))
+print(json.dumps(json.loads(value), indent=4, sort_keys=True))
