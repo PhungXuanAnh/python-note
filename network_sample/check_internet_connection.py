ab1d050f957e662565eb12001c007fe3d91643b7
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-07-31 14:07:09 +0700

    update module check connection

--- network_sample/check_internet_connection.py
+++ network_sample/check_internet_connection.py
@@ -1,15 +1,27 @@
 import socket
-REMOTE_SERVER = "www.google.com"
-def internet_is_connected():
+
+
+def internet_is_connected(domain, port=80):
     try:
         # see if we can resolve the host name -- tells us if there is
         # a DNS listening
-        host = socket.gethostbyname(REMOTE_SERVER)
+        host = socket.gethostbyname(domain)
         # connect to the host -- tells us if the host is actually
         # reachable
-        socket.create_connection((host, 80), 2)
+        socket.create_connection((host, port), 2)
         return True
     except:
-        pass
-    return False
-print (internet_is_connected())
+        return False
+
+
+print(internet_is_connected("www.google.com"))
+
+
+def check_connection(host, port):
+    try:
+        socket.create_connection((host, port), 2)
+        return True
+    except:
+        return False
+
+print(check_connection('localhosta', 2181))
