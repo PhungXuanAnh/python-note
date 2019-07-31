ee7a0acb1a97b233b515af1287259cba0af85374
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-07-31 14:52:38 +0700

    format json save data sample

--- json_handle/json_save_data.py
+++ json_handle/json_save_data.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 import json
 
-dict_data= {
-        "path":"abc",
-        "list_deployment": []
-        }
+dict_data = {
+    "path": "abc",
+    "list_deployment": []
+}
 
 file_path = "test.json"
 
-with open(file_path,"w") as out_file:
+with open(file_path, "w") as out_file:
     # Save the dictionary into this file
     # (the 'indent=4' is optional, but makes it more readable)
-    json.dump(dict_data,out_file, indent=4)                                    
-    
+    json.dump(dict_data, out_file, indent=4)
