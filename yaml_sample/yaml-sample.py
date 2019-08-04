acd32e182979c35e9cd05465f1f04434ee0a5ac1
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-08-04 10:11:25 +0700

    update sample for yaml file

--- yaml_sample/yaml-sample.py
+++ yaml_sample/yaml-sample.py
@@ -1,29 +1,15 @@
-'''
-Created on Mar 27, 2017
+import yaml
+import json
 
-@author: xuananh
-'''
-import yaml, json
-
-# with open('/media/xuananh/data/Dropbox/Viosoft/Eclipse_workspace/Validium-nfv-mano3/samples/heat-validium1.yaml', 'r') as f:
-#     doc = yaml.load(f)
-#     print(json.dumps(doc, indent=4, sort_keys=True))
-#     
-# with open('sample.json', 'w') as out_file:    
-#     json.dump(doc,out_file, indent=4)
-#     
-# with open('sample.yaml', 'w') as out_file:
-#     yaml.dump(doc, out_file)
-
-with open('/media/xuananh/data/Dropbox/Viosoft/Eclipse_workspace/Validium-nfv-mano3/samples/heat-validium1.yaml', 'r') as f:
+# read
+with open('input.yaml', 'r') as f:
     doc = yaml.load(f)
-#     doc = yaml.safe_load(f)
     print(json.dumps(doc, indent=4, sort_keys=True))
-    
-with open('sample1.json', 'w') as out_file:    
-    json.dump(doc,out_file, indent=4)
-    
-with open('sample1.yaml', 'w') as out_file:
+
+# write to yaml file
+with open('output.yaml', 'w') as out_file:
     yaml.dump(doc, out_file)
-    
-    
+
+# write to json file
+with open('output.json', 'w') as out_file:
+    json.dump(doc, out_file, indent=4)
