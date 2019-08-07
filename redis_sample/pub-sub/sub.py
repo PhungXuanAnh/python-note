0025c52be2d87f39f1a72bd45304b1294d67f610
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-08-07 11:37:22 +0700

    update redis pub sub sample

--- redis_sample/pub-sub/sub.py
+++ redis_sample/pub-sub/sub.py
@@ -6,17 +6,31 @@ import json
 channel1 = 'test'
 
 
-def sub(r, name):
+def sub1(r, name):
     pubsub = r.pubsub()
     pubsub.subscribe([channel1])
     for item in pubsub.listen():
-        print('%s : %s' % (name, item['data']))
+        print('%s : %s' % (name, item))
         if item['data'] == b'5':
             pubsub.unsubscribe()
+            return item['data']
+
+
+def sub2(r, name):
+    pubsub = r.pubsub()
+    pubsub.subscribe([channel1])
+    while True:
+        message = pubsub.get_message()
+        print('%s : %s' % (name, message))
+        if message and message['data'] == b'5':
+            break
+        time.sleep(1)
+    return message
 
 
 r = redis.StrictRedis(host='localhost', port=6379, db=10)
 
-Process(target=sub, args=(r, 'reader1')).start()
-Process(target=sub, args=(r, 'reader2')).start()
-sub(r, 'reader0')
+Process(target=sub1, args=(r, 'reader1')).start()
+# result = sub2(r, 'reader2')
+result = sub1(r, 'reader11')
+print('result: {}'.format(result))
