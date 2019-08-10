2c228f6a2ba9e9edb7e5a2076cfd0fa7a81703f5
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-08-10 12:57:59 +0700

    update logging sample

--- logging_sample/handler/stream_handler.py
+++ logging_sample/handler/stream_handler.py
@@ -1,14 +1,22 @@
 import logging
 import sys
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    format='%(levelname)s - %(message)s',
                     filename='/tmp/stream_handler_test.txt',
                     filemode='a')
 
 my_handler = logging.StreamHandler(sys.stdout)
 my_handler.setLevel(logging.INFO)
-my_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+my_handler.setFormatter(logging.Formatter('%(name)s: %(asctime)s - %(levelname)s - %(message)s'))
+
+logging.info('aaaaaaaaaaaaaa dont add handler')
 logging.getLogger('').addHandler(my_handler)
+logging.info('aaaaaaaaaaaaaa added handler')
 
 
-logging.info('aaaaaaaaaaaaaa')
+logger = logging.getLogger('my-log')
+logger.setLevel(logging.INFO)
+logger.info('bbbbbbbbbbbbb dont add handler')
+
+logger.addHandler(my_handler)
+logger.info('bbbbbbbbbbbbb added handler')
