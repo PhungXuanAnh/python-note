9a205a26a047cf04cdf4c1a4ca8ed4d4b5a28662
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-08-17 12:33:12 +0700

    change code

--- file_directory/folder_and_file.py
+++ file_directory/folder_and_file.py
@@ -4,35 +4,35 @@ import shutil
 
 # Find out current working directory #
 script_dir = os.path.dirname(__file__)
-print (script_dir)
+print(script_dir)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print (dir_path)
+print(dir_path)
 
 current_working_dir = os.getcwd()
-print (current_working_dir)
+print(current_working_dir)
 
 # Check file or folder exist?
 if os.path.exists('test_dir'):
-    print ("File is exist")
+    print("File is exist")
 else:
-    print ("File is NOT exist")
+    print("File is NOT exist")
 
 # Check file exist
-print (os.path.isfile("path"))
-print (not os.path.isfile("path"))
+print(os.path.isfile("path"))
+print(not os.path.isfile("path"))
 
 # Check folder exist? If not creat it #
 if not os.path.exists('/home/xuananh/data/Temp/test1/test2'):
     os.makedirs('/home/xuananh/data/Temp/test1/test2')
 
-time.sleep(5)
+time.sleep(1)
 try:
     # remove a file. #
     os.remove("/home/xuananh/data/Temp/test1")
 except Exception as e:
     print('111111111111 ', e)
-    print (e)
+    print(e)
 
 try:
     # remove an empty directory. #
@@ -44,8 +44,10 @@ try:
     # delete a directory and all its contents. #
     shutil.rmtree("/home/xuananh/data/Temp/test1")
 except Exception as e:
-    print('333333333333333 ', e)        
+    print('333333333333333 ', e)
 
-print (os.getcwd())
-print (os.path.exists("../abc_module"))
-print (os.path.join("a", "b", "c"))
+print(os.getcwd())
+print(os.path.exists("../abc_module"))
+print(os.path.join("a", "b", "c"))
+print(os.path.basename('/users/system1/student1/homework-1.py'))
+print(os.path.dirname('/users/system1/student1/homework-1.py'))
