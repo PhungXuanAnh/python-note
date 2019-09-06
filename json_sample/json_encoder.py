bccdf2dffd0a9595eb22b7c1ae8c31bc032a39f4
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-09-06 10:50:27 +0700

    .

---
+++ json_sample/json_encoder.py
@@ -0,0 +1,30 @@
+import json
+import datetime
+from decimal import Decimal
+from bson import ObjectId
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.timedelta):
+            return (datetime.datetime.min + obj).time().isoformat()
+        else:
+            return super(DateTimeEncoder, self).default(obj)
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+class ObjectIdEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
--- mongodb_sample/utils.py
+++ mongodb_sample/utils.py
@@ -1,7 +1,6 @@
 import json
 from bson import ObjectId
 
-
 class ObjectIdEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
