26bec18edca9e19bb248a0ad16d3f0ec3c82a21f
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-10-25 11:50:14 +0700

    .

--- redis_sample/pub-sub/redis_pub_sub.py
+++ redis_sample/pub-sub/redis_pub_sub.py
@@ -3,22 +3,39 @@ import time
 import redis
 import json
 
+timeout = 5
+
 
 def pub(r):
     for n in range(10):
         r.publish('channel', 'blah %d' % n)
-        time.sleep(5)
+        time.sleep(1.5)
+    return
 
 
-def sub(r, name):
-    pubsub = r.pubsub()
-    pubsub.subscribe(['channel'])
-    for item in pubsub.listen():
-        print('%s : %s' % (name, item['data']))
+def sub_listen(r, name):
+    p = r.pubsub()
+    p.subscribe(['channel'])
+    for item in p.listen():
+        print('%s : %s' % (name, item))
+
+
+def sub_get_message(r, name):
+    p = r.pubsub()
+    p.subscribe(['channel'])
+    start = time.time()
+    while True:
+        message = p.get_message()
+        if message:
+            print('%s : %s' % (name, message))
+        time.sleep(0.01)
+        if time.time() - start > timeout:
+            print('timeout happened, stop!')
+            return
 
 
 if __name__ == '__main__':
     r = redis.StrictRedis(host='localhost', port=6379, db=1)
     Process(target=pub, args=(r,)).start()
-    Process(target=sub, args=(r, 'reader1')).start()
-    Process(target=sub, args=(r, 'reader2')).start()
+    Process(target=sub_listen, args=(r, 'reader1')).start()
+    Process(target=sub_get_message, args=(r, 'reader2')).start()
