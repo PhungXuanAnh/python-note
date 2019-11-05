7af4f9afc8386f7f29132c839a4bfd907d7c704c
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-11-05 10:50:09 +0700

    .

---
+++ mongodb_sample/mongo_url.py
@@ -0,0 +1,5 @@
+AUTHEN_URL = 'mongodb://{user}:{password}@{hostname}:{port}/'.format(
+    user='mongoadmin',
+    password='secret',
+    hostname='localhost',
+    port=27018)
--- mongodb_sample/pymongo_sample.py
+++ mongodb_sample/pymongo_sample.py
@@ -1,7 +1,7 @@
 import pymongo
 from mongo_url import AUTHEN_URL
 
-# client = pymongo.MongoClient('localhost', 27018)
+client = pymongo.MongoClient('localhost', 27018)
 client = pymongo.MongoClient(AUTHEN_URL)
 print(client.server_info())    # NOTE: this statement can be used to check connection to mongodb server
 
