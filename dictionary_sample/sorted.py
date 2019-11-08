b25b3f6d15d765103b320178a98d24a8711eaec5
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-11-08 17:57:24 +0700

    .

--- dictionary_sample/sorted.py
+++ dictionary_sample/sorted.py
@@ -5,23 +5,23 @@ from collections import OrderedDict
 
 x = {'value2': 2, 'value3': 4, 'value3': 3, 'value1': 1, 'value0': 0}
 
-print('==========================================================')
+print('========================================================== 0')
 sorted_x = sorted(x.items(), key=operator.itemgetter(1))
 print(json.dumps(sorted_x, indent=4, sort_keys=True))
 
-print('==========================================================')
+print('========================================================== 1')
 sorted_by_value = sorted(x.items(), key=lambda kv: kv[1])
 print(json.dumps(sorted_by_value, indent=4, sort_keys=True))
 
-print('==========================================================')
+print('========================================================== 2')
 s = [(k, x[k]) for k in sorted(x, key=x.get, reverse=True)]
 print(json.dumps(s, indent=4, sort_keys=True))
 
-print('==========================================================')
+print('========================================================== 3')
 data_sorted = {k: v for k, v in sorted(x.items(), key=lambda y: y[1])}
 print(json.dumps(data_sorted, indent=4, sort_keys=True))
 
-print('==========================================================')
+print('========================================================== 4')
 list1 = [
     {'name': 'Homer', 'age': 39},
     {'name': 'Yart', 'age': 10},
@@ -33,13 +33,15 @@ newlist = sorted(list1, key=lambda k: k['name'])
 print(newlist)
 newlist = sorted(list1, key=lambda k: k['age'])
 print(newlist)
+newlist = sorted(list1, key=lambda k: k['age'], reverse=True)
+print(newlist)
 newlist = sorted(list1, key=itemgetter('name'))
 print(newlist)
 newlist = sorted(list1, key=itemgetter('age'))
 print(newlist)
 newlist = sorted(list1, key=itemgetter('age'), reverse=True)
 
-print('==========================================================')
+print('========================================================== 5')
 
 dict1 = {
     "17292": 5000,
@@ -56,7 +58,7 @@ sorted_by_value_ascending = sorted(dict1.items(), key=lambda kv: kv[1])
 print(sorted_by_value_descending)
 print(sorted_by_value_ascending)
 
-print('==========================================================')
+print('========================================================== 6')
 
 
 d = {'123': {'key1': 3, 'key2': 11, 'key3': 3},
