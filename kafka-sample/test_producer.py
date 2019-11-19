d383f6f7dc1bbb8d9be6f39b1b93839fb38c883d
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-11-19 10:12:13 +0700

    .

--- kafka-sample/test_producer.py
+++ kafka-sample/test_producer.py
@@ -4,14 +4,15 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
 topic = 'my-topic'
-# kafka_servers = ['localhost:9092']
-kafka_servers = ['167.71.198.165:9092']
+kafka_servers = ['localhost:9092']
+# kafka_servers = ['167.71.198.165:9092']
 
 
 def on_send_success(record_metadata):
     print('topic:     {}'.format(record_metadata.topic))
     print('partition: {}'.format(record_metadata.partition))
     print('offset:    {}'.format(record_metadata.offset))
+
 
 producer = KafkaProducer(
     bootstrap_servers=kafka_servers,
