7a2c7bff08a240e2246437fcd0e2d2267a2aea8c
Phung Xuan Anh <phungxuananh1991@gmail.com>
2020-02-01 11:31:44 +0700

    .

--- network_sample/check_remote_server_on.py
+++ network_sample/check_remote_server_on.py
@@ -1,9 +1,11 @@
 import socket
 import datetime
+
+
 def is_remote_server_on(server_ip, port=22):
     print(server_ip, port)
     start = datetime.datetime.now()
-    
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((server_ip, port))
@@ -18,5 +20,51 @@ def is_remote_server_on(server_ip, port=22):
         print("time to check: {}".format((now - start).seconds))
         s.close()
         return False
-    
-print (is_remote_server_on("10.80.200.23"))
+
+
+def internet_is_connected(domain, port=80):
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(domain)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        socket.create_connection((host, port), 2)
+        return True
+    except Exception:
+        traceback.print_exc()
+        return False
+
+
+def check_connection(host, port):
+    try:
+        socket.create_connection((host, port), 10)
+        return True
+    except Exception:
+        traceback.print_exc()
+        return False
+
+
+def check_ping_server(server_ip):
+    import shlex
+    import subprocess
+
+    # Tokenize the shell command
+    # cmd will contain  ["ping","-c1","google.com"]
+
+    # cmd=shlex.split("ping -c1 google.com")
+    cmd = shlex.split("ping -c1 {}".format(server_ip))
+    try:
+        output = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        # Will print the command failed with its exit status
+        print("The IP {0} is Not Reachable".format(cmd[-1]))
+    else:
+        print("The IP {0} is Reachable".format(cmd[-1]))
+
+
+if __name__ == "__main__":
+    print(check_connection('localhost', 22))
+    print(internet_is_connected("www.google.com"))
+    print(is_remote_server_on("localhost"))
+    check_ping_server('localhost')
--- network_sample/get_ip.py
+++ network_sample/get_ip.py
@@ -1,22 +1,61 @@
 import socket
 import fcntl
 import struct
+import traceback
 
-def get_ip_address_interface(ifname):
+
+def get_ip_address_interface1(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+    try:
+        ip = socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', bytes(ifname[:15], 'utf-8'))
+        )[20:24])
+        print("IP of '{}' is {}".format(ifname, ip))
+        return ip
+    except OSError:
+        print("No such device: {} to get IP".format(ifname))
+        return None
+    except Exception:
+        traceback.print_exc()
+        return None
 
-print (get_ip_address_interface('enp2s0'))
 
-
-def get_ip_address():
+def get_ip_which_can_connect_to_internet1():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     s.connect(("8.8.8.8", 80))
+    # s.connect(("8.8.8.8", 80))
     s.connect(("google.com", 80))
-    return s.getsockname()[0]
+    ip = s.getsockname()[0]
+    print("IP addresses which can connect to internet: {}".format(ip))
+    return ip
 
-print (get_ip_address())
+
+def get_ip_which_can_connect_to_internet2():
+    from pyroute2 import IPRoute
+    ip_addresses = []
+    with IPRoute() as ipr:
+        routes = ipr.route('get', dst='8.8.8.8')
+        for value in list(routes):
+            for v in value['attrs']:
+                if v[0] == 'RTA_PREFSRC':
+                    ip = v[1]
+                    print("IP addresses which can connect to internet: {}".format(ip))
+                    return ip
+
+
+def get_all_interface_and_their_ip():
+    from netifaces import interfaces, ifaddresses, AF_INET
+    for ifaceName in interfaces():
+        addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
+        print('%s: %s' % (ifaceName, ', '.join(addresses)))
+
+
+if __name__ == "__main__":
+    get_ip_which_can_connect_to_internet1()
+    print('----------------------------------')
+    get_ip_which_can_connect_to_internet2()
+    print('----------------------------------')
+    get_ip_address_interface1('wlo1')
+    print('----------------------------------')
+    get_all_interface_and_their_ip()
