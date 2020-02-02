ed7b92cbadc6273522fc3222a9bad9ae9683a600
Phung Xuan Anh <phungxuananh1991@gmail.com>
2020-02-02 15:46:01 +0700

    .

--- logging_sample/level/custom_level.py
+++ logging_sample/level/custom_level.py
@@ -3,14 +3,14 @@ import sys
 
 # DEBUG_LEVELV_NUM = 21
 # logging.addLevelName(DEBUG_LEVELV_NUM, "DEBUGV")
-#  
+#
 # def logger_func(self, message, *args, **kws):
 #     if self.isEnabledFor(DEBUG_LEVELV_NUM):
-#         self._log(DEBUG_LEVELV_NUM, message, args, **kws) 
-#  
+#         self._log(DEBUG_LEVELV_NUM, message, args, **kws)
+#
 # setattr(logging.Logger, "debugv", logger_func)
-# 
-# 
+#
+#
 # def logging_func(msg, *args, **kwargs):
 #     """
 #     Log a message with severity 'DEBUG' on the root logger.
@@ -19,8 +19,9 @@ import sys
 #     if len(root.handlers) == 0:
 #         logging.basicConfig()
 #     root.debugv(msg, *args, **kwargs)
-#     
+#
 # setattr(sys.modules[logging.__name__], "debugv", logging_func)
+
 
 def add_logging_level(LOGGING_FUNC_NAME="debugv", LEVEL_NAME='DEBUGV', LEVEL_NUM=25):
     '''
@@ -35,24 +36,23 @@ def add_logging_level(LOGGING_FUNC_NAME="debugv", LEVEL_NAME='DEBUGV', LEVEL_NUM
                             NOTSET = 0
     '''
     logging.addLevelName(LEVEL_NUM, LEVEL_NAME)
-    
+
     def logger_func(self, message, *args, **kws):
         if self.isEnabledFor(LEVEL_NUM):
-            self._log(LEVEL_NUM, message, args, **kws) 
-     
+            self._log(LEVEL_NUM, message, args, **kws)
+
     setattr(logging.Logger, LOGGING_FUNC_NAME, logger_func)
-    
-    
+
     def logging_func(msg, *args, **kwargs):
         root = logging.root
         if len(root.handlers) == 0:
             logging.basicConfig()
         getattr(root, LOGGING_FUNC_NAME)(msg, *args, **kwargs)
-        
+
     setattr(sys.modules[logging.__name__], LOGGING_FUNC_NAME, logging_func)
 
 
-#========================= test
+# ========================= test
 add_logging_level()
 
 logging.basicConfig(level=logging.INFO)
@@ -64,4 +64,3 @@ logging.debugv("ccccccccc")
 
 logger1 = logging.getLogger('logger1')
 logger1.debugv('111111111111111')
-
