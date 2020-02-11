ce97fee996a0479b0a56c26e6e8456eefdd9595f
Phung Xuan Anh <phungxuananh1991@gmail.com>
2020-02-11 15:51:48 +0700

    .

--- yaml_sample/yaml-sample.py
+++ yaml_sample/yaml-sample.py
@@ -3,12 +3,12 @@ import json
 
 # read
 with open('input.yaml', 'r') as f:
-    doc = yaml.load(f)
+    doc = yaml.safe_load(f)
     print(json.dumps(doc, indent=4, sort_keys=True))
 
 # write to yaml file
 with open('output.yaml', 'w') as out_file:
-    yaml.dump(doc, out_file)
+    yaml.safe_dump(doc, out_file)
 
 # write to json file
 with open('output.json', 'w') as out_file:
