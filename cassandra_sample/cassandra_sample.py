a96986581c68440a7f262d986b1c87abf4424af5
Phung Xuan Anh <phungxuananh1991@gmail.com>
2020-03-16 17:20:25 +0700

    .

--- cassandra_sample/cassandra_sample.py
+++ cassandra_sample/cassandra_sample.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-
+# NOTE: reference: https://docs.datastax.com/en/developer/python-driver/3.22/getting_started/
 def cassandra_conn(db_name):
     auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
     cluster = Cluster(['127.0.0.1'], port=9042, auth_provider=auth_provider)
