b1c2b2d23dacc521948c639418ef1c8d1c2cc42c
Phung Xuan Anh <phungxuananh1991@gmail.com>
2020-07-25 18:15:12 +0700

    Add sample remote debug by debugpy

---
+++ debug_sample/debugpy_sample/debugpy_sample.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''
+Hướng dẫn:
+    1. thay địa chỉ remote server của dòng comment bên dưới
+    2. thay đổi [host, port, localRoot, remoteRoot] trong file .vscode/launch.json
+    3. chọn breakpoint, có 2 cách:
+        3.1. Cách 1: chọn breakpoint trực tiếp bằng cái chấm màu đỏ,
+            - khuyến khích dùng cách này,
+            - LƯU Ý: breakpoint trên dòng bị comment hoặc dòng trắng không có tác dụng 
+        3.2. Cách 2: dùng debugpy.breakpoint() như bên dưới 
+    4. chạy file này
+    5. vào mục Run của vscode, chọn `Python: Remote Attach`
+    6. Click Run button 
+'''
+import time
+import multiprocessing
+
+
+def worker():
+    print("====================== worker")
+    
+    import debugpy
+    # debugpy.listen(("0.0.0.0", 5678))
+    debugpy.listen(5678)
+    debugpy.wait_for_client()
+    # debugpy.breakpoint()
+
+    i = 0
+    while i < 10:
+        print(i)
+        i = i + 1
+
+
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=worker)
+    p1.start()
+
+    a = "aaa-"
+    b = "bbb-"
+    c = "ccc"
+    final = a + b + c
+    print(final)
+    
