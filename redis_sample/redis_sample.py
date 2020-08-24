a509b6b49a421d55466bb6393da8eccfec149379
xuananh <xuan.anh.phung@advesa.com>
2020-08-24 16:16:05 +0700

    Refactoring redis_sample.py

--- redis_sample/redis_sample.py
+++ redis_sample/redis_sample.py
@@ -1,33 +1,56 @@
 import redis
+import json
 
-# it will create its own connection
 r = redis.StrictRedis(host='localhost', port=6379, db=1)
-r.set('set2', 'bar')
-print(r.get('set2'))
+def connect_directly():
+    r0 = redis.StrictRedis(host='localhost', port=6379, db=1)
+    r0.set('set2', 'bar')
+    print(r0.get('set2'))
 
-# using 1 connection for multiple client
-pool = redis.ConnectionPool(host='localhost', port=6379, db=2)
-r = redis.Redis(connection_pool=pool)
-r.set('set1', 'set1value')
+def connect_using_pool():
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=2)
+    r1 = redis.Redis(connection_pool=pool)
+    r1.set('pool_sample', 'this is value of pool sample key')
+    print(r1.get('pool_sample'))
 
-# using pipeline to call multi command to server
-pool = redis.ConnectionPool(host='localhost', port=6379, db=3)
-r = redis.Redis(connection_pool=pool)
-r.set('bing', 'baz')
-# Use the pipeline() method to create a pipeline instance
-pipe = r.pipeline()
-# The following SET commands are buffered
-pipe.set('foo', 'bar')
-pipe.get('bing')
-# the EXECUTE call sends all buffered commands to the server, returning
-# a list of responses, one for each command.
-print(pipe.execute())
-# or combine all above command in one
-print(pipe.set('foo', 'bar').sadd('faz', 'baz').incr('auto_number').execute())
+def pipeline():
+    # using pipeline to call multi command to server
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=3)
+    r2 = redis.Redis(connection_pool=pool)
+    r2.set('bing', 'baz')
+    # Use the pipeline() method to create a pipeline instance
+    pipe = r.pipeline()
+    # The following SET commands are buffered
+    pipe.set('foo', 'bar')
+    pipe.get('bing')
+    # the EXECUTE call sends all buffered commands to the server, returning
+    # a list of responses, one for each command.
+    print(pipe.execute())
+    # or combine all above command in one
+    print(pipe.set('foo', 'bar').sadd('faz', 'baz').incr('auto_number').execute())                  
+
+def set_bit():
+    r.setbit('set_bit_sample', 3, 1)
+    print(r.get('set_bit_sample'))
+
+def check_key_exist():
+    print(r.exists('unknow_key'))
+    
+    r.set("know_key", "value")
+    print(r.exists('know_key'))
+
+def save_a_dict():
+    mydict = { 'var1' : 5, 'var2' : 9, 'var3': [1, 5, 9] }
+    key = "save_a_dict"
+    r.set(key, json.dumps(mydict))
+    print(r.get(key))
+    print(json.loads(r.get(key)))
 
 
-pool = redis.ConnectionPool(host='localhost', port=6379, db=4)
-r = redis.Redis(connection_pool=pool)
-r.setbit('mykey', 3, 1)
-
-print(r.exists('aaa'))
+if __name__ == "__main__":
+    # connect_directly()
+    # connect_using_pool()
+    # pipeline()
+    # set_bit()
+    # check_key_exist()
+    save_a_dict()
