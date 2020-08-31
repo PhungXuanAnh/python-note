2ed99f2eb5888873a2a23ff7cf3f9a4d07afe4ab
xuananh <xuan.anh.phung@advesa.com>
2020-08-31 16:12:39 +0700

    Add name to formater logging

--- logging_sample/logging_dictConfig.py
+++ logging_sample/logging_dictConfig.py
@@ -39,7 +39,7 @@ LOGGING = {
     },
     'formatters': {
         'verbose': {
-            'format': "[%(asctime)s] [%(custom_format)s] [%(module)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s"
+            'format': "[%(asctime)s] [%(custom_format)s] [%(name)s] [%(module)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
