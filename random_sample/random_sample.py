3522233e64144bef6374660709afa5ddd97edc01
xuananh <xuan.anh.phung@advesa.com>
2021-04-08 17:45:26 +0700

    Add random datetime sample

--- random_sample/random_sample.py
+++ random_sample/random_sample.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 print('choose 1 random number in range 0 - 9: ', random.randint(0, 9))
 
@@ -12,3 +13,10 @@ print(random.sample(range(0, 10), 3))
 print(random.sample(range(0, 10), 3))
 print(random.sample(range(0, 10), 4))
 print(random.sample(range(0, 10), 5))
+
+start_timestamp = time.mktime(time.strptime('Jun 1 2010  01:33:00', '%b %d %Y %I:%M:%S'))
+end_timestamp = time.mktime(time.strptime('Jun 1 2017  12:33:00', '%b %d %Y %I:%M:%S'))
+def randomize_time(start_timestamp,end_timestamp):
+    print(time.strftime('%b %d %Y %I:%M:%S', time.localtime(random.randrange(start_timestamp,end_timestamp))))
+    # return time.strftime('%b %d %Y %I:%M:%S', time.localtime(random.randrange(start_timestamp,end_timestamp)))
+randomize_time(start_timestamp,end_timestamp)
