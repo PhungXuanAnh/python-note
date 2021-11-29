f70247272d07af63e2fd494874e3bc4e9f796ac9
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-11-29 16:26:00 +0700

    .

--- list_sample/list_sort.py
+++ list_sample/list_sort.py
@@ -2,6 +2,10 @@ list_to_be_sorted = [
     {'name':'ddd', 'age':39}, 
     {'name':'aaa', 'age':10}, 
     {'name':'bbb', 'age':20}, 
-    {'name':'ccc', 'age':40}]
+    {'name':'ccc', 'age':40},
+]
 
-print(sorted(list_to_be_sorted, key=lambda k: k['age']))    # reference: https://stackoverflow.com/a/73050/7639845
+ # reference: https://stackoverflow.com/a/73050/7639845
+print(sorted(list_to_be_sorted, key=lambda k: k['age']))   
+print(type(sorted(list_to_be_sorted, key=lambda k: k['age'])))
+
