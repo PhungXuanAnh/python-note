fa01321a752e13da64f7b5e6a3f6a1c4a047b473
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-03-31 11:00:22 +0700

    update sample format currency

--- currency_format/currency_format.py
+++ currency_format/currency_format.py
@@ -6,15 +6,17 @@ def using_locale_lib():
     # locale.setlocale( locale.LC_ALL, 'en_CA.UTF-8' )
     locale.setlocale( locale.LC_ALL, 'vi_VN.utf8' )
     print(locale.currency(1346896.67444, grouping=True, symbol=True))
-    print(locale.currency(10000000, grouping=True, symbol=True))
-    print(locale.currency(10000000, grouping=True, symbol=True, international=True))
+    print('Currency in vietnamese: ', locale.currency(10000000, grouping=True, symbol=True))
+    print('Currency in English     ', locale.currency(10000000, grouping=True, symbol=True, international=True))
     print(locale.format_string('%.2f', 10000000, True))
 
 
 def using_string_format():
     print('\n---------------------------- format currency using_string_format --------------------')
-    print("{:,.2f}VND".format(10000000))
-    print("{:,.0f}VND".format(10000000))
+    print("Separated by comma ',' : {:,.2f}VND".format(10000000))
+    print("Separated by comma ',' : {:,.0f}VND".format(10000000))
+    print("Separated by dot   '.' : {:,.2f}VND".format(10000000).replace(',', '.'))
+    print("Separated by dot   '.' : {:,.0f}VND".format(10000000).replace(',', '.'))
     print("{:,.0f}VND".format(0))
 
 
