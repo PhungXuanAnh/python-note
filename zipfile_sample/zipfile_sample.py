6e5dd8541b37363d6fc768952c14636673d696e0
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-05-23 15:38:12 +0700

    add sample zip file

--- zipfile_sample/zipfile_sample.py
+++ zipfile_sample/zipfile_sample.py
@@ -1,9 +1,10 @@
+import os
 import zipfile
 import logging
 import datetime
 
-path_to_zip_file = "/media/xuananh/data/Downloads/plugin/ping-nsb1.zip"
-directory_to_extract_to = '/media/xuananh/data/Downloads'
+path_to_zip_file = "/home/xuananh/data/Downloads/plugin/ping-nsb1.zip"
+directory_to_extract_to = '/home/xuananh/data/Downloads'
 
 def extractall(path_to_zip_file, directory_to_extract_to):
     try:
@@ -19,14 +20,14 @@ def print_info(archive_name):
     print("Infor of archive file: ")
     zf = zipfile.ZipFile(archive_name)
     for info in zf.infolist():
-        print info.filename
-        print '\tComment:\t', info.comment
-        print '\tModified:\t', datetime.datetime(*info.date_time)
-        print '\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)'
-        print '\tZIP version:\t', info.create_version
-        print '\tCompressed:\t', info.compress_size, 'bytes'
-        print '\tUncompressed:\t', info.file_size, 'bytes'
-        print     
+        print(info.filename)
+        print('\tComment:\t', info.comment)
+        print('\tModified:\t', datetime.datetime(*info.date_time))
+        print('\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)')
+        print('\tZIP version:\t', info.create_version)
+        print('\tCompressed:\t', info.compress_size, 'bytes')
+        print('\tUncompressed:\t', info.file_size, 'bytes')
+
 
 def retrieve_archive_member_info(archive_file, member_list):
     zf = zipfile.ZipFile(path_to_zip_file, 'r')    
@@ -34,19 +35,31 @@ def retrieve_archive_member_info(archive_file, member_list):
         try:
             info = zf.getinfo(filename)
         except KeyError:
-            print 'ERROR: Did not find %s in zip file' % filename
+            print('ERROR: Did not find %s in zip file' % filename)
         else:
-            print '%s is %d bytes' % (info.filename, info.file_size)
-            
+            print('%s is %d bytes' % (info.filename, info.file_size))
+
+
+
+def zipfolder(the_name_of_the_zipfile="test_zipfile", 
+              the_directory_to_be_zipped="/home/xuananh/repo/python-note/unittest_sample"):            
+    zipobj = zipfile.ZipFile(the_name_of_the_zipfile + '.zip', 'w', zipfile.ZIP_DEFLATED)
+    rootlen = len(the_directory_to_be_zipped) + 1
+    for base, dirs, files in os.walk(the_directory_to_be_zipped):
+        for file in files:
+            fn = os.path.join(base, file)
+            zipobj.write(fn, fn[rootlen:])
+
+      
     
 if __name__ == '__main__':
-    print("File is zip format: {}".format(zipfile.is_zipfile(path_to_zip_file)))
+    # print("File is zip format: {}".format(zipfile.is_zipfile(path_to_zip_file)))
 
-    zf = zipfile.ZipFile(path_to_zip_file, 'r')    
-    print("Files name of the files in archive: {}".format(zf.namelist()))
+    # zf = zipfile.ZipFile(path_to_zip_file, 'r')    
+    # print("Files name of the files in archive: {}".format(zf.namelist()))
     
-    print_info(path_to_zip_file)
+    # print_info(path_to_zip_file)
     
-    retrieve_archive_member_info(path_to_zip_file, [ 'README.txt', 'ping.yaml', 'ping-nsb1/ping.py' ])
+    # retrieve_archive_member_info(path_to_zip_file, [ 'README.txt', 'ping.yaml', 'ping-nsb1/ping.py' ])
     
-    
+    zipfolder()
