42d08bd3740ba5f59a267b2edf43aa2e6779c789
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-09-09 10:16:07 +0700

    update samplelogging

--- logging_sample/logging_dictConfig.py
+++ logging_sample/logging_dictConfig.py
@@ -1,3 +1,28 @@
+'''
+Format of logging
+Attribute name      Format                    Description
+args               You shouldn't need to format this yourself.The tuple of arguments merged into msg to produce message, or a dict whose values are used for the merge (when there is only one argument, and it is a dictionary).
+asctime            %(asctime)s    Human-readable time when the LogRecord was created. By default this is of the form '2003-07-08 16:49:45,896' (the numbers after the comma are millisecond portion of the time).
+created            %(created)f    Time when the LogRecord was created (as returned by time.time()).
+exc_info           You shouldn't need to format this yourself.Exception tuple (sys.exc_info) or, if no exception has occurred, None.
+filename           %(filename)s    Filename portion of pathname.
+funcName           %(funcName)s    Name of function containing the logging call.
+levelname          %(levelname)s    Text logging level for the message ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL').
+levelno            %(levelno)s    Numeric logging level for the message (DEBUG, INFO, WARNING, ERROR, CRITICAL).
+lineno             %(lineno)d    Source line number where the logging call was issued (if available).
+module             %(module)s    Module (name portion of filename).
+msecs              %(msecs)d    Millisecond portion of the time when the LogRecord was created.
+message            %(message)s    The logged message, computed as msg % args. This is set when Formatter.format() is invoked.
+msg                You shouldn't need to format this yourself.The format string passed in the original logging call. Merged with args to produce message, or an arbitrary object (see Using arbitrary objects as messages).
+name               %(name)s    Name of the logger used to log the call.
+pathname           %(pathname)s    Full pathname of the source file where the logging call was issued (if available).
+process            %(process)d    Process ID (if available).
+processName        %(processName)s    Process name (if available).
+relativeCreated    %(relativeCreated)d    Time in milliseconds when the LogRecord was created, relative to the time the logging module was loaded.
+thread             %(thread)d    Thread ID (if available).
+threadName         %(threadName)s    Thread name (if available).
+'''
+
 from logging.config import dictConfig
 import logging
 import os
@@ -39,7 +64,7 @@ LOGGING = {
     },
     'formatters': {
         'verbose': {
-            'format': "[%(asctime)s] [%(custom_format)s] [%(name)s] [%(module)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s"
+            'format': "[%(asctime)s] [%(custom_format)s] [%(name)s:%(funcName)s] [%(pathname)s:%(lineno)d] %(levelname)s: %(message)s"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
