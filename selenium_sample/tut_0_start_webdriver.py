b8508480c87bdea48c9ffe77082e83a5a2d20efd
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-06-30 17:09:15 +0700

    .

--- selenium_sample/tut_0_start_webdriver.py
+++ selenium_sample/tut_0_start_webdriver.py
@@ -1,25 +1,16 @@
-import time
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
 
 
 # chrome driver download from link: https://chromedriver.storage.googleapis.com/index.html?path=2.44/
 
 chrome_options = ChromeOptions()
 chrome_options.add_experimental_option("detach", True)  # keep chrome and chromedriver to stay open after running all code
+# chrome_options.add_argument('--profile-directory=Profile 16')
+# chrome_options.add_argument("user-data-dir=/home/xuananh/.config/google-chrome1") #Path to your chrome profile
 
 service = Service(executable_path='webdriver/chromedriver')
 driver = Chrome(service=service,options=chrome_options)
 driver.maximize_window()
-driver.get("http://viettuts.vn")
-
-web_menu = driver.find_element(By.XPATH, ".//a[@href='/web']")
-
-action = ActionChains(driver)
-action.move_to_element(web_menu)
-action.perform()
+driver.get("https://translate.google.com/?sl=vi&tl=en&op=translate")
--- selenium_sample/tut_interact_check_box.py
+++ selenium_sample/tut_interact_check_box.py
@@ -1,21 +1,22 @@
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
 
 url = "https://www.calculator.net/mortgage-calculator.html"
 chrome_path = '/home/xuananh/Downloads/chromedriver_linux64/chromedriver'
 # chrome driver download from link: https://chromedriver.storage.googleapis.com/index.html?path=2.44/
 
 chrome_option = ChromeOptions()
-driver = Chrome(executable_path=chrome_path,
-                options=chrome_option)
+service = Service(executable_path='webdriver/chromedriver')
+driver = Chrome(service=service,options=chrome_option)
 driver.maximize_window()
 driver.get(url)
 
 # click on Radio Button
-driver.find_element_by_id('caddoptional').click()
+driver.find_element(By.ID, 'caddoptional').click()
 print('Is Selected:  ', driver.find_element_by_id('caddoptional').is_selected())
 print('Is Enabled:   ', driver.find_element_by_id('caddoptional').is_enabled())
 print('Is Displayed: ', driver.find_element_by_id('caddoptional').is_displayed())
