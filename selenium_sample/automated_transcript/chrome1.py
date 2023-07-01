81e72e124a55c87ac8d04296572451f6e5abaded
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-07-01 09:09:50 +0700

    .

--- selenium_sample/automated_transcript/chrome1.py
+++ selenium_sample/automated_transcript/chrome1.py
@@ -9,7 +9,16 @@ chrome_options = ChromeOptions()
 # chrome_options.add_experimental_option("detach", True)  # keep chrome and chromedriver to stay open after running all code
 # chrome_options.add_argument('--profile-directory=Profile 16')
 chrome_options.add_argument("user-data-dir=/home/xuananh/.config/google-chrome1") #Path to your chrome profile
-# chrome_options.add_argument("user-data-dir=/home/xuananh/.config/google-chrome2") #Path to your chrome profile
+chrome_options.add_argument("class=chrome1")    # NOTE: it must add this argument while running chrome to allow new chrome instance
+                                                # be linked to new icon and be grouped in another icon in dock of ubuntu launcher
+                                                # for example this is command to run chrome in terminal:
+                                                # google-chrome --user-data-dir=/home/xuananh/.config/google-chrome1 --class="chrome1"
+                                                # Add StartupWMClass=chrome1 to this file :
+                                                # /home/xuananh/Dropbox/Work/Other/conf.d/google-chrome/desktop-shortcut/chrome1.desktop 
+                                                # copy or create soft link to ~/.local/share/applications/
+                                                # open new chrome shortcut from menu of launcher to see new icon in dock
+                                                # then run this python file
+                                                # reference: https://askubuntu.com/a/367851/1077704
 
 service = Service(executable_path='webdriver/chromedriver')
 driver = Chrome(service=service,options=chrome_options)
--- selenium_sample/automated_transcript/chrome2.py
+++ selenium_sample/automated_transcript/chrome2.py
@@ -8,9 +8,21 @@ from selenium.webdriver.common.by import By
 chrome_options = ChromeOptions()
 # chrome_options.add_experimental_option("detach", True)  # keep chrome and chromedriver to stay open after running all code
 # chrome_options.add_argument('--profile-directory=Profile 16')
-chrome_options.add_argument("user-data-dir=/home/xuananh/.config/google-chrome2") #Path to your chrome profile
+chrome_options.add_argument("user-data-dir=/home/xuananh/.config/google-chrome2")
+chrome_options.add_argument("class=chrome2")    # NOTE: it must add this argument while running chrome to allow new chrome instance
+                                                # be linked to new icon and be grouped in another icon in dock of ubuntu launcher
+                                                # for example this is command to run chrome in terminal:
+                                                # google-chrome --user-data-dir=/home/xuananh/.config/google-chrome1 --class="chrome1"
+                                                # Add StartupWMClass=chrome1 to this file :
+                                                # /home/xuananh/Dropbox/Work/Other/conf.d/google-chrome/desktop-shortcut/chrome1.desktop 
+                                                # copy or create soft link to ~/.local/share/applications/
+                                                # open new chrome shortcut from menu of launcher to see new icon in dock
+                                                # then run this python file
+                                                # reference: https://askubuntu.com/a/367851/1077704
 
-service = Service(executable_path='webdriver/chromedriver')
+service = Service(
+    executable_path='webdriver/chromedriver',
+    service_args=["--verbose", "--log-path=/home/xuananh/Downloads/chromedriver.log"])
 driver = Chrome(service=service,options=chrome_options)
 driver.maximize_window()
 driver.get("https://translate.google.com/?sl=vi&tl=en")
@@ -23,7 +35,7 @@ while True:
         voice_button = driver.find_element(By.XPATH, '//button[@aria-label="Translate by voice" and @aria-pressed="false"]')
         voice_button.click()
     except:
-        traceback.print_exc()
+        # traceback.print_exc()
         time.sleep(1)
     
 
