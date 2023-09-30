4bd667c75b9b65824e2e8f14a95535c1e07d7ceb
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-09-30 23:20:33 +0700

    .

--- selenium_sample/automated_transcript/savvycom_logtime.py
+++ selenium_sample/automated_transcript/savvycom_logtime.py
@@ -9,22 +9,45 @@ from selenium.webdriver.support import expected_conditions as EC
 reference: https://stackoverflow.com/a/51265680/7639845
 
 download and install chrome-desktop-stable.deb
-sudo apt install xvfb -y
+sudo apt install xvfb libatk-bridge2.0-0 -y
 
 install ffmpeg version  4.4.2 or higher
 sudo apt install ffmpeg -y
 or higher version https://askubuntu.com/a/1360862/1077704
 
+------------ test
 xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 1920x1080x24" \
     /home/xuananh/repo/python-note/.venv/bin/python \
     /home/xuananh/repo/python-note/selenium_sample/automated_transcript/savvycom_logtime.py &
 
-xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth \
-    -s "-ac -screen 0 1920x1080x24" .venv/bin/python savvycom_logtime.py &
-export DISPLAY=:44
-rm -rf video.mp4 && ffmpeg -f x11grab -video_size 1920x1080 -i :44 -codec:v libx264 -r 12 video.mp4
+---------------- vim run.sh
 
-ctrl+c to stop capture video
+xvfb-run --listen-tcp --server-num 44 \
+    --auth-file /tmp/xvfb.auth \
+    -s "-ac -screen 0 1920x1080x24" \
+    /home/ubuntu/.Dropbox/test/.venv/bin/python \
+    /home/ubuntu/.Dropbox/test/savvycom_logtime.py &
+
+export DISPLAY=:44
+
+ffmpeg -f x11grab -video_size 1920x1080 -i :44 -codec:v libx264 -r 12 \
+    /home/ubuntu/.Dropbox/test/video/$(date +'%Y-%m-%d__%H-%M-%S').mp4
+
+s castnet-prod-ubuntu-user
+
+crontab -e
+# chay 5p 1 lan
+*/5 * * * * /home/ubuntu/.Dropbox/test/run.sh
+
+crontab -l
+
+# remove crontab
+crontab -r
+
+sudo systemctl restart cron
+systemctl status cron
+
+TODO: remove video after 1 month to avoid disk issue
 """
 
 chrome_options = webdriver.ChromeOptions()
@@ -33,10 +56,9 @@ driver = webdriver.Chrome(options=chrome_options)
 driver = webdriver.Chrome()
 driver.get("https://timesheet.savvycom.xyz/")
 
-login_button = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//button[@value="login"]')))
 user_name = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//input[@id="Username"]'))).send_keys("anh.phung")
 password = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//input[@id="Password"]'))).send_keys("2721991@Savvycom")
-login_button.click()
+login_button = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//button[@value="login"]'))).click()
 remote_check_time_button = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//button/span[text()="Remote Check in/out"]'))).click()
 
 for i in range(0, 15):
